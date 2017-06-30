c207eadd341f8806de509e9e1f0029e01c753103
Alexandr Em <Alexandr Em>
2017-06-30 18:09:37 +0300

    Создано приложение backend

--- ishop/settings.py
+++ ishop/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'ishop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ishop',
+        'USER': 'postgres',
+        'PASSWORD': 'ghjuhfvvbcn96',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
